b074e0baa42c1560c2672c76a1cd25630aeec7c7
technillogue <technillogue@gmail.com>
2021-06-21 20:20:44 -0400

    merge instructions

--- run.sh
+++ run.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 xhost +local:docker
 sudo docker run --net=host --env="DISPLAY" --volume="$HOME/.Xauthority:/root/.Xauthority:rw" -it secondsignal signal-desktop --no-sandbox               
+# save the qr code
+# zbarimg --quiet $qr_file | cut --delimiter ':' --fields 2- > signal_uri
+# ./signal-cli addDevice --uri $(cat signal_uri)
--- setup.sh
+++
@@ -1,5 +0,0 @@
-# open signal
-# save the qr code
-# zbarimg
-a
-# ./signal-cli addDevice --uri
